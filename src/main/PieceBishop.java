3325f6c01ef07a8ce206510df776d854ca9c5466
asdf <github@champgm.e4ward.com>
2013-03-17 14:55:38 -0500

    bishop done
    It's not as efficient as the Rook, but it's literally the exact same
    concept and even uses VaidBreak, which I was hesitant to move up into
    ChessPiece.

--- src/main/PieceBishop.java
+++ src/main/PieceBishop.java
@@ -10,7 +10,70 @@ public class PieceBishop extends ChessPiece{
 	
 	@Override
 	Vector<ChessSquare> calcPossibleMoves() {
-		// TODO Auto-generated method stub
-		return null;
+		System.out.println("Bishop: Calculating possible moves from "+xPos+","+yPos);
+		Vector<ChessSquare> possibleMoves=new Vector<ChessSquare>();
+		//northeast
+		for(int i=1;i<8;i++){
+			int x=xPos+i;
+			int y=yPos+i;
+			System.out.println("Rook: loop1 checking "+x+","+y);
+			boolean[] validBreak=ValidBreak(x,y);
+			if(validBreak[0]){
+				possibleMoves.add(myBoard.getSquareAt(x, y));
+				if(validBreak[1]){
+					break;
+				}
+			}else{
+				break;
+			}
+		}
+		//southeast
+		for(int i=1;i<8;i++){
+			int x=xPos+i;
+			int y=yPos-i;
+			System.out.println("Rook: loop1 checking "+x+","+y);
+			boolean[] validBreak=ValidBreak(x,y);
+			if(validBreak[0]){
+				possibleMoves.add(myBoard.getSquareAt(x, y));
+				if(validBreak[1]){
+					break;
+				}
+			}else{
+				break;
+			}
+		}
+		//southwest
+		for(int i=1;i<8;i++){
+			int x=xPos-i;
+			int y=yPos-i;
+			System.out.println("Rook: loop1 checking "+x+","+y);
+			boolean[] validBreak=ValidBreak(x,y);
+			if(validBreak[0]){
+				possibleMoves.add(myBoard.getSquareAt(x, y));
+				if(validBreak[1]){
+					break;
+				}
+			}else{
+				break;
+			}
+		}
+		//northwest
+		for(int i=1;i<8;i++){
+			int x=xPos-i;
+			int y=yPos+i;
+			System.out.println("Rook: loop1 checking "+x+","+y);
+			boolean[] validBreak=ValidBreak(x,y);
+			if(validBreak[0]){
+				possibleMoves.add(myBoard.getSquareAt(x, y));
+				if(validBreak[1]){
+					break;
+				}
+			}else{
+				break;
+			}
+		}
+		
+		
+		return possibleMoves;
 	}
 }
